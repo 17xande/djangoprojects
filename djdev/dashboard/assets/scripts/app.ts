5965e18e411ecb160126251fcea900b87d816b5f
Alexandre Figueiredo <17xande@gmail.com>
2024-05-28 22:40:22 +0200

    starting to get a look

--- djdev/dashboard/assets/scripts/app.ts
+++ djdev/dashboard/assets/scripts/app.ts
@@ -1,5 +1,5 @@
-
 import { setBasePath } from "@shoelace-style/shoelace"
+import '@shoelace-style/shoelace/dist/components/card/card.js'
 
 // Set the base path to the folder you copied Shoelace's assets to.
 setBasePath('/assets')
